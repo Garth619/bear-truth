de8e82979c99fd81d89c3a05baa3d27b7455e075
garrett cullen <info@garrettcullen.com>
2016-05-09 21:16:33 -0700

    push

---
+++ wp-content/themes/beartruth/page-paul.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * Template Name: Paul Page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+
+
+<div class="inner_banner">
+	
+	<img src="<?php bloginfo('template_directory');?>/images/banner.png"/>
+	
+</div><!-- inner_banner -->
+
+<div class="inner_content">
+	
+	<div id="content">
+
+	<h1>Title</h1>
+	<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui o cia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ex- ercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui o cia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, con- sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excep- teur sint occaecat cupidatat non proident, sunt in culpa qui o cia deserunt mollit anim id est laborum.</p>
+	
+	
+	</div><!-- #content -->
+	
+	
+	
+	
+</div><!-- innner_content -->
+
+<div class="paul_video">
+		
+		<div class='embed-container'>
+			<iframe src='https://www.youtube.com/embed/lwSJ9nWCi5I' frameborder='0' allowfullscreen></iframe>
+		</div>
+		
+	</div><!-- paul_video -->
+	
+	<div class="paul_link">
+		
+	<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg"/>
+	
+	</div><!-- paul_link -->
+	
+	<div class="characters">
+		
+		<h1>Characters</h1>
+		
+		<div class="single_character">
+			
+			<div class="charater_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+			
+			</div><!-- charater_img_wrapper -->
+			
+			<div class="charater_content_wrapper">
+				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				
+			</div><!-- charater_content_wrapper -->
+			
+		</div><!-- single_character -->
+		
+		<div class="single_character">
+			
+			<div class="charater_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+			
+			</div><!-- charater_img_wrapper -->
+			
+			<div class="charater_content_wrapper">
+				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				
+			</div><!-- charater_content_wrapper -->
+			
+		</div><!-- single_character -->
+		
+		<div class="single_character">
+			
+			<div class="charater_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+			
+			</div><!-- charater_img_wrapper -->
+			
+			<div class="charater_content_wrapper">
+				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				
+			</div><!-- charater_content_wrapper -->
+			
+		</div><!-- single_character -->
+		
+		<div class="single_character">
+			
+			<div class="charater_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+			
+			</div><!-- charater_img_wrapper -->
+			
+			<div class="charater_content_wrapper">
+				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				
+			</div><!-- charater_content_wrapper -->
+			
+		</div><!-- single_character -->
+		
+		<div class="single_character">
+			
+			<div class="charater_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+			
+			</div><!-- charater_img_wrapper -->
+			
+			<div class="charater_content_wrapper">
+				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				
+			</div><!-- charater_content_wrapper -->
+			
+		</div><!-- single_character -->
+		
+		<div class="single_character">
+			
+			<div class="charater_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+			
+			</div><!-- charater_img_wrapper -->
+			
+			<div class="charater_content_wrapper">
+				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				
+			</div><!-- charater_content_wrapper -->
+			
+		</div><!-- single_character -->
+		
+	</div><!-- characters -->
+	
+	<div class="inner_banner">
+	
+	<img src="<?php bloginfo('template_directory');?>/images/banner.png"/>
+	
+</div><!-- inner_banner -->
+
+<div class="paul_boxes">
+	
+	<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+	<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+	<img src="<?php bloginfo('template_directory');?>/images/paul-link.jpg">
+	
+</div><!-- paul_boxes -->
+
+
+<div class="main_feed">
+
+
+</div><!-- main_feed -->
+
+
+
+
+
+
+
+<?php get_footer(); ?>
