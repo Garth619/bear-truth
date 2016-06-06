64644aaeae71435f54bf5687594f570b0296b922
garrett cullen <info@garrettcullen.com>
2016-06-05 22:14:10 -0700

    push

--- wp-content/themes/beartruth/sidebar.php
+++ wp-content/themes/beartruth/sidebar.php
@@ -8,4 +8,18 @@
  */
 ?>
 
-		<div class="sidebar"></div><!-- sidebar -->
+		<div class="sidebar">
+			
+			
+			
+			<h2>Sign up for awesome freebies, contests, and special&nbsp;offers</h2>
+ 
+	  <div class="form">
+		  <?php gravity_form( 2, false, false, false, '', true );?>
+		 </div><!-- form -->
+	
+
+			
+			
+			
+		</div><!-- sidebar -->
