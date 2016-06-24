db1cbfe12945bea20307e0e8f0674476d637eb27
garrett cullen <info@garrettcullen.com>
2016-06-24 11:06:55 -0700

    push

--- wp-content/themes/beartruth/404.php
+++ wp-content/themes/beartruth/404.php
@@ -9,14 +9,14 @@
 
 get_header(); ?>
 
-	<div id="container">
+	<div id="container" style="height:350px;padding:45px 0 0 10px;">
 		<div id="content" role="main">
 
 			<div id="post-0" class="post error404 not-found">
 				<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
 				<div class="entry-content">
-					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
-					<?php get_search_form(); ?>
+					<p><?php _e( 'Apologies, but the page you requested could not be found', 'twentyten' ); ?></p>
+					
 				</div><!-- .entry-content -->
 			</div><!-- #post-0 -->
 
